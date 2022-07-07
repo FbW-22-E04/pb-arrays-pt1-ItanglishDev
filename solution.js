66e975320e34bd75e578b8434b461fa3859c4296
chris <chrishethertonweb@gmail.com>
2022-07-07 12:09:44 +0200

    1 to 3 done

--- solution.js
+++ solution.js
@@ -1,1 +1,7 @@
+const euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+const secondCity = euroCities[1];
 
+euroCities.splice(0, 1, "Berlin");
+
+console.log(euroCities);
+console.log(euroCities.length);
